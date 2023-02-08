8d443dc6a03ba410efffe507df0f00073facf319
Prince86 <pkbghl90@gmail.com>
2023-02-08 18:31:07 +0530

    Authentication added. next-auth, prisma

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,13 +1,25 @@
-
 // import { Inter } from '@next/font/google'
-
+import { signIn, signOut, useSession } from "next-auth/react";
 
 // const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const { data, status } = useSession();
+
+  console.log(data);
   return (
     <>
-      Hello
+      {data?.user?.name? (
+        <>
+        {data?.user?.name}
+        <button onClick={() => signOut()}>Sign Out</button>
+        </>
+      ) : (
+        <>
+          <button onClick={() => signIn("google")}>Sign In With Google</button>
+          <button onClick={() => signIn("github")}>Sign In With Github</button>
+        </>
+      )}
     </>
-  )
+  );
 }
